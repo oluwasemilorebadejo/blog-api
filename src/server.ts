b964cbf28dbc380ae94b8cf3ad4f00fcfa4b8c6c
blac <badejo.semilore.7@gmail.com>
2023-09-04 10:43:41 +0100

    about to start database models

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import dotenv from "dotenv";
 
+process.on("uncaughtException", (err: Error) => {
+  console.log("UNCAUGHT EXCEPTION, SHUTTING DOWN...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" }); // has to be set before requiring app because the env vars have to be set so the app module has access to it also
 
 createConnection()
@@ -16,4 +22,13 @@ const port: string | undefined = process.env.PORT;
 
 const server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
+});
+
+process.on("uncaughtException", (err: Error) => {
+  console.log("UNCAUGHT EXCEPTION, SHUTTING DOWN...");
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
