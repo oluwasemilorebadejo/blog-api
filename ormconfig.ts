e6e551b8266b301a052b2c1e7f6dd4bdca876411
blac <badejo.semilore.7@gmail.com>
2023-09-10 14:43:30 +0100

    updated ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,4 +1,7 @@
 import { DataSourceOptions } from "typeorm";
+import path from "path";
+
+const entityPath = path.resolve(__dirname, "..", "entities");
 
 const config: DataSourceOptions = {
   type: "postgres", // Database type (e.g., postgres, mysql, sqlite)
@@ -7,7 +10,7 @@ const config: DataSourceOptions = {
   username: process.env.DATABASE_USERNAME, // Database username
   password: process.env.DATABASE_PASSWORD, // Database password
   database: process.env.DATABASE_NAME, // Database name
-  entities: ["dist/src/**/*/*.entity{.ts,.js}"], // Entity files path (you'll need to adjust this)
+  entities: [`${entityPath}/*.{js,ts}`], // Entity files path (you'll need to adjust this)
   synchronize: true, // Automatically sync database schema on application start
   logging: false, // Enable SQL query logging
   ssl: true, // Enable SSL
