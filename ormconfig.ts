d04b3f2ccb9fd7ba77c4d901a4cb97d565226a00
blac <badejo.semilore.7@gmail.com>
2023-09-10 14:26:50 +0100

    updated

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,7 +7,7 @@ const config: DataSourceOptions = {
   username: process.env.DATABASE_USERNAME, // Database username
   password: process.env.DATABASE_PASSWORD, // Database password
   database: process.env.DATABASE_NAME, // Database name
-  entities: ["src/entities/*.ts"], // Entity files path (you'll need to adjust this)
+  entities: ["src/entities/*.{ts,js}"], // Entity files path (you'll need to adjust this)
   synchronize: true, // Automatically sync database schema on application start
   logging: false, // Enable SQL query logging
   ssl: true, // Enable SSL
